91b248ffd3ff7bf490e56aca872b850b65b5d6d9
Jordan Taylor <jtaylor007.jt@gmail.com>
2016-12-21 20:31:40 -0500

    binary search tree

---
+++ binary-search-tree.py
@@ -0,0 +1,32 @@
+# Code snippet for the binary search tree validation problem in hackerrank
+# Link : https://www.hackerrank.com/challenges/ctci-is-binary-search-tree
+
+""" Node is defined as
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+"""
+def tree_max(node):
+    if not node:
+        return float("-inf")
+    maxleft  = tree_max(node.left)
+    maxright = tree_max(node.right)
+    return max(node.data, maxleft, maxright)
+
+def tree_min(node):
+    if not node:
+        return float("inf")
+    minleft  = tree_min(node.left)
+    minright = tree_min(node.right)
+    return min(node.data, minleft, minright)
+
+def check_binary_search_tree_(root):
+    if not root:
+        return True
+    if (tree_max(root.left) <= root.data <= tree_min(root.right) and
+        check_binary_search_tree_(root.left) and check_binary_search_tree_(root.right)):
+        return True
+    else:
+        return False
