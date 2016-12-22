4e581fb15771cd823dd8efd6cea2845107181060
Jordan Taylor <jtaylor007.jt@gmail.com>
2016-12-22 18:32:42 -0500

    almost done

--- bettwosets.go
+++ bettwosets.go
@@ -29,17 +29,34 @@ func main() {
 	third, _ := reader.ReadString('\n')
 
 	sizes := convertToIntArray(first)
+	_ = sizes
 	a := convertToIntArray(second)
 	b := convertToIntArray(third)
+	comb := append(a, b...)
 
-	fmt.Println(sizes)
-	fmt.Println(a)
-	fmt.Println(b)
+	validator := checkDenominator(a, b)
+
+	factorMap := map[int]int{}
+
+	if validator == true {
+		for _, i := range comb {
+			factorArray := numberFactors(i)
+			for _, j := range factorArray {
+				factorMap[j] += 1
+			}
+		}
+	} else {
+		fmt.Println(0)
+		return
+	}
+
+	count := countFactors(factorMap, a)
+	fmt.Println(count)
 }
 
 func convertToIntArray(s string) []int {
 	array := make([]int, 0)
-	trim := strings.TrimSpace(s)
+	trim := strings.TrimSpace(s) // hackerrank adds newline spacing must trim
 	str := strings.Split(trim, " ")
 
 	for _, i := range str {
@@ -50,4 +67,37 @@ func convertToIntArray(s string) []int {
 		array = append(array, j)
 	}
 	return array
+}
+
+func checkDenominator(a []int, b []int) bool {
+	for i := range b {
+		for j := range a {
+			if b[i]%a[j] != 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func numberFactors(n int) []int {
+	array := make([]int, 0)
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			array = append(array, i)
+		}
+	}
+	return array
+}
+
+func countFactors(m map[int]int, a []int) int {
+	array := make([]int, 0)
+	for key, val := range m {
+		for _, i := range a {
+			if val > 1 && key >= i {
+				array = append(array, val)
+			}
+		}
+	}
+	return len(array)
 }
