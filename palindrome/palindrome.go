aa47e6f15634af1ab12942ed164a00fb189a4715
Jordan Taylor <jtaylor007.jt@gmail.com>
2017-02-08 08:18:57 -0500

    palindrome validation function done

---
+++ palindrome/palindrome.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	s := []string{"blutulb", "test", "  ", "racecar", "do  od", "lefties"}
+
+	for _, v := range s {
+		check := validatePalidrome(v)
+		fmt.Printf("Word: %s\n    %t\n", v, check)
+	}
+}
+
+func validatePalidrome(s string) (b bool) {
+	word := strings.Split(s, "")
+	first := 0
+	last := len(word) - 1
+
+	for {
+		if last <= first {
+			break
+		}
+
+		if word[first] == word[last] {
+			b = true
+			first++
+			last--
+		} else {
+			b = false
+			break
+		}
+	}
+
+	return
+}
